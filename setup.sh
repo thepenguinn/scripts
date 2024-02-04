fa21a3ee5820e5b6782ffa7d265bdbfa6c562a67
Daniel V Mathew <danielvmathew27@gmail.com>
2024-02-04 18:02:44 +0530

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+Bin_Dir="${HOME}/.local/bin"
+
+mkdir "${Bin_Dir}" -p > /dev/null 2>&1
+
+ln -s "${PWD}/epubconv/epubconv" "${Bin_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/gen-cover-img/gen-cover-img" "${Bin_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/kdb/kdb" "${Bin_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/smu/smu" "${Bin_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/splitpdf/splitpdf" "${Bin_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/viewpdf/viewpdf" "${Bin_Dir}" > /dev/null 2>&1

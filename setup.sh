ee9cfa536e45b2babf5194820f2b11f29abc9381
Daniel V Mathew <danielvmathew27@gmail.com>
2024-04-25 10:46:20 +0530

    tss: now tss is properly working and added tsl
    added tsl (tmux-sessionizer-launcher) which decides
    to switch to or create a tss session

--- setup.sh
+++ setup.sh
@@ -11,3 +11,4 @@ ln -s "${PWD}/smu/smu" "${Bin_Dir}" > /dev/null 2>&1
 ln -s "${PWD}/splitpdf/splitpdf" "${Bin_Dir}" > /dev/null 2>&1
 ln -s "${PWD}/viewpdf/viewpdf" "${Bin_Dir}" > /dev/null 2>&1
 ln -s "${PWD}/tss/tss" "${Bin_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/tss/tsl" "${Bin_Dir}" > /dev/null 2>&1

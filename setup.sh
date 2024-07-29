0fb0f619ae64a61fe705867f6bc49e0714e92068
Daniel V Mathew <danielvmathew27@gmail.com>
2024-07-29 05:48:47 +0530

    updated setup.sh
    now setup.sh will also make the script executable

--- setup.sh
+++ setup.sh
@@ -4,14 +4,19 @@ Bin_Dir="${HOME}/.local/bin"
 
 mkdir "${Bin_Dir}" -p > /dev/null 2>&1
 
-ln -s "${PWD}/epubconv/epubconv" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/gen-cover-img/gen-cover-img" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/kdb/kdb" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/smu/smu" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/splitpdf/splitpdf" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/viewpdf/viewpdf" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/tss/tss" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/tss/tsl" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/tss/tsm" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/glrclib/glrclib" "${Bin_Dir}" > /dev/null 2>&1
-ln -s "${PWD}/perc/perc" "${Bin_Dir}" > /dev/null 2>&1
+chmod_and_link () {
+    chmod +x "$1"
+    ln -s "$1" "$2" > /dev/null 2>&1
+}
+
+chmod_and_link "${PWD}/epubconv/epubconv" "${Bin_Dir}"
+chmod_and_link "${PWD}/gen-cover-img/gen-cover-img" "${Bin_Dir}"
+chmod_and_link "${PWD}/kdb/kdb" "${Bin_Dir}"
+chmod_and_link "${PWD}/smu/smu" "${Bin_Dir}"
+chmod_and_link "${PWD}/splitpdf/splitpdf" "${Bin_Dir}"
+chmod_and_link "${PWD}/viewpdf/viewpdf" "${Bin_Dir}"
+chmod_and_link "${PWD}/tss/tss" "${Bin_Dir}"
+chmod_and_link "${PWD}/tss/tsl" "${Bin_Dir}"
+chmod_and_link "${PWD}/tss/tsm" "${Bin_Dir}"
+chmod_and_link "${PWD}/glrclib/glrclib" "${Bin_Dir}"
+chmod_and_link "${PWD}/perc/perc" "${Bin_Dir}"
